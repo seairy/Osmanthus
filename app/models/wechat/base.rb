0a02fa1b1c1bc9949c7d19438c066c0f5534c378
seairy <seairy@gmail.com>
2016-01-10 20:36:02 +0800

    init commit

--- app/models/wechat/base.rb
+++ app/models/wechat/base.rb
@@ -17,12 +17,13 @@ class Wechat::Base < ActiveRecord::Base
 
     def find_access_token
       uri = URI.parse("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{Setting.key[:wechat][:appid]}&secret=#{Setting.key[:wechat][:secret]}")
-      http = Net::HTTP.new(uri.host, 443)
-      http.use_ssl = true
-      request = Net::HTTP::Get.new(uri.request_uri)
-      response = http.request(request)
-      json = JSON.parse(response.body)
-      first ? update_all(access_token: json['access_token'], expired_at: Time.now + json['expires_in'].seconds) : create!(access_token: json['access_token'], expired_at: Time.now + json['expires_in'].seconds)
+      request = Net::HTTP::Get.new(uri)
+      response = JSON.parse(response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http.ssl_version = :SSLv3
+        http.request(request)
+      end.body)
+      first ? update_all(access_token: response['access_token'], expired_at: Time.now + response['expires_in'].seconds) : create!(access_token: response['access_token'], expired_at: Time.now + response['expires_in'].seconds)
       first
     end
 
